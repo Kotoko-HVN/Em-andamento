94ba4e636f3f6acd1adc01523b834ff57efd3b40
kotoko <Robsonpizza219@gmail.com>
2025-04-17 14:52:00 -0300

    Create Investimento.c

---
+++ Investimento.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float capital, montante, tempo, juros;
+    
+    printf("Digite o capital inicial: ");
+    scanf("%f", &capital);
+    printf("Digite por quanto tempo ficará rendendo (em meses): ");
+    scanf("%f", &tempo);
+    printf("Digite a taxa de juros mensal: ");
+    scanf("%f", &juros);
+    
+    juros = ()
+    
+    
+    
+    
+    
+    montante = (juros + capital);
+    printf("O valor total dos juros somados ao capital existente é de: %.2f", montante);
+    
+    return 0;
+}
