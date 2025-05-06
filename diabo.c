fa4b5aac518aefff8c6dea884e911fa6b80fb681
kotoko <Robsonpizza219@gmail.com>
2025-05-06 11:50:17 -0300

    Update diabo.c

--- diabo.c
+++ diabo.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> 
 
-struct agenda {
-    int celular;
-    char nome[50];
+struct contato {
+    char nome [50];
+    char telefone [50];
+    char email [50];
 };
 
 
-int main()
-{
-   struct agenda A1;
-   
-  strcpy(A1.nome, "robson");
-  A1.celular = 23;
 
-printf("Nome do contato: %s\n", A1.nome);
-printf("Numero do contato: %d\n", A1.celular);
+int main() { 
+    int n, i;
+    
+    printf("Quantos contatos deseja cadastrar? ");
+    scanf("%d", &n);
+    
+struct contato ListaDeContatos[n];
+
+  for (i = 0; i < n; i++) {
+        printf("\nCadastro do contato %d\n", i + 1);
+        getchar();
+        
+    printf("Nome: ");
+    fgets(ListaDeContatos[i].nome, sizeof(ListaDeContatos[i].nome), stdin);
+    ListaDeContatos[i].nome[strcspn(ListaDeContatos[i].nome, "\n")] = 0;
+    
+    printf("Telefone: ");
+    fgets(ListaDeContatos[i].nome, sizeof(ListaDeContatos[i].nome), stdin);
+    ListaDeContatos[i].nome[strcspn(ListaDeContatos[i].telefone, "\n")] = 0;
+    
+    printf("Email: ");
+    fgets(ListaDeContatos[i].nome, sizeof(ListaDeContatos[i].nome), stdin);
+    ListaDeContatos[i].nome[strcspn(ListaDeContatos[i].email, "\n")] = 0;
+    
+    
+    printf("\nLista de contatos \n");
+    
+    
+    for (i = 0; i < n; i++);
+    printf("\nContato %d\n", i + 1);
+    printf("\nTelefone %s\n", i + 1);
+    printf("\nEmail %s\n", i + 1);
+
+}
 
     return 0;
 }
